8f6e9e466752dfad66f3df8f2a5cb75135fca199
Joshua Farmer <rhubarbjosh@outlook.com>
2026-02-04 18:04:39 +0000

    oop

--- util/run.sh
+++ util/run.sh
@@ -3,8 +3,9 @@ if grub-file --is-x86-multiboot bin/kernel; then
         mkdir -p isodir/boot/grub
         cp bin/kernel isodir/boot/kernel.bin
         cp grub.cfg isodir/boot/grub/grub.cfg
-        sudo mount fat32.img mnt
-        sudo rm -rf mnt/
+        mkdir -p mnt
+        sudo mount bin/fat32.img mnt
+        sudo rm -rf mnt/*
         sudo cp fat32dir/* mnt/ -rf
         sudo umount mnt
         grub-mkrescue -o bin/dekos.iso isodir
