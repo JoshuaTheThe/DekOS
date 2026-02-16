6d7e1e3b50766957afb411ed5167dc205d503869
Joshua Farmer <rhubarbjosh@outlook.com>
2026-02-16 21:29:32 +0000

    aaaa

--- programs/API/make.sh
+++ programs/API/make.sh
@@ -1,5 +1,5 @@
-clang -m32 -march=i386 -I. -c -ffreestanding -msoft-float -fno-builtin "ini.c" -o "ini.o" -Wall -Wextra
-clang -m32 -march=i386 -I. -c -ffreestanding -msoft-float -fno-builtin "stdio.c" -o "stdio.o" -Wall -Wextra
-clang -m32 -march=i386 -I. -c -ffreestanding -msoft-float -fno-builtin "string.c" -o "string.o" -Wall -Wextra
-clang -m32 -march=i386 -I. -c -ffreestanding -msoft-float -fno-builtin "dek.c" -o "dek.o" -Wall -Wextra
+clang -m32 -march=i386 -I. -c -ffreestanding -msoft-float -fno-builtin "ini.c" -o "ini.o" -Wall -Wextra -fPIC
+clang -m32 -march=i386 -I. -c -ffreestanding -msoft-float -fno-builtin "stdio.c" -o "stdio.o" -Wall -Wextra -fPIC
+clang -m32 -march=i386 -I. -c -ffreestanding -msoft-float -fno-builtin "string.c" -o "string.o" -Wall -Wextra -fPIC
+clang -m32 -march=i386 -I. -c -ffreestanding -msoft-float -fno-builtin "dek.c" -o "dek.o" -Wall -Wextra -fPIC
 clang -m32 -march=i386 -I. -c "start.s" -o "start.o" -Wall -Wextra
